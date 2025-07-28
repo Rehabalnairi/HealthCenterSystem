be90b08c1943c66502365b35774a89e4c090c304
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-07-28 08:42:22 +0400

    update class department

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HealthCenterSystem.Models
 {
-    class Booking
+    public class Booking
     {
-       public int PationtId { get; set; } // property to hold patient ID
-        public int DepId { get; set; }
-        public int DoctorId { get; set; } // property to hold doctor ID
-        public int BranchId { get; set; } // property to hold branch ID
-        public int ClinicId { get; set; } // property to hold clinic ID
+        public int PatientId { get; set; } // property to hold patient ID
+        public int DepId { get; set; }     // property to hold department ID
+        public int DoctorId { get; set; }  // property to hold doctor ID
+        public int BranchId { get; set; }  // property to hold branch ID
+        public int ClinicId { get; set; }  // property to hold clinic ID
         public DateTime BookingDate { get; set; } // property to hold booking date
-        
-        public class Dooking
+    }
+
+    public static class BookingData
+    {
+        public static List<Department> Departments = new List<Department>
         {
-            public static List<Department> Departments = new List<Department>(); // static list to hold departments
-            {
-                Departments.Add(new Department(1, "Cardiology")); // add a department to the list
-                Departments.Add(new Department(2, "Neurology")); // add another department to the list
-                };
-        public static List<Doctor> doctors = new List<Doctor>()
-        {
-
-
-        new Doctor { Id = 1, Name = "Dr. Smith", DepartmentId = 1 },
-        new Doctor { Id = 2, Name = "Dr. Jones", DepartmentId = 2 }
-
+            new Department { Id = 1, Name = "Cardiology" },
+            new Department { Id = 2, Name = "Neurology" }
         };
-        public static List<DateTime> AvailableTimes = new List<DateTime>();
 
+        public static List<Doctor> Doctors = new List<Doctor>
+        {
+            new Doctor { Id = 1, Name = "Dr. Smith", DepartmentId = 1 },
+            new Doctor { Id = 2, Name = "Dr. Jones", DepartmentId = 2 }
+        };
 
+        public static List<DateTime> AvailableTimes = new List<DateTime>
+        {
+            DateTime.Today.AddHours(9),
+            DateTime.Today.AddHours(10),
+            DateTime.Today.AddHours(11)
+        };
     }
 }
--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HealthCenterSystem.Models
 {
@@ -11,24 +8,24 @@ namespace HealthCenterSystem.Models
         public int DepId { get; set; }
         public string DepName { get; set; }
         public List<Doctor> Doctors { get; set; } // list to hold doctors in the department
-        public List<Clinic> clinics { get; set; } // list to hold clinics in the department
+        public List<Clinic> Clinics { get; set; } // list to hold clinics in the department
 
         public Department(int depId, string depName) // constructor to initialize department properties
         {
             this.DepId = depId;
             this.DepName = depName;
             this.Doctors = new List<Doctor>(); // initialize the list of doctors
-            this.clinics = new List<Clinic>(); // initialize the list of clinics
+            this.Clinics = new List<Clinic>(); // initialize the list of clinics
         }
 
         public void AddDoctor(Doctor doctor) // method to add a doctor to the department
         {
-            this.Doctors.Add(doctor); // add the doctor to the list of doctors
+            this.Doctors.Add(doctor);
         }
 
         public void AddClinic(Clinic clinic) // method to add a clinic to the department
         {
-            this.clinics.Add(clinic); // add the clinic to the list of clinics
+            this.Clinics.Add(clinic);
         }
     }
 }
