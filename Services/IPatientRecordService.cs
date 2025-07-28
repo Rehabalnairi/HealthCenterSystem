1f7ca68ad19d2a5970efb3543d9e86d3fa671cfd
Thuraiya <3eya151@gmail.com>
2025-07-28 09:26:21 +0400

    create IPatientRecordService interface that contain Add Patient Record, Update Patient Record and Delete Patient Record also contain three list

--- Services/IPatientRecordService.cs
+++ Services/IPatientRecordService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthCenterSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace HealthCenterSystem.Services
 {
     interface IPatientRecordService
     {
+        void AddPatientRecord(PatientRecord record); // method to add a patient record
+        void UpdatePatientRecord(int recordId, Patient patient, Doctor doctor, DateTime visitDate, string diagnosis, string treatment, string notes); // method to update a patient record
+        PatientRecord GetPatientRecordById(int recordId); // method to get a patient record by ID
+        List<PatientRecord> GetAllPatientRecords(); // method to get all patient records
+
+        List<PatientRecord> GetRecordsByPatientId(int patientId); // method to get records by patient ID
+        List<PatientRecord> GetRecordsByDoctorId(int doctorId); // method to get records by doctor ID
+        void DeletePatientRecord(int recordId); // method to delete a patient record
     }
 }
