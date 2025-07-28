eec563bcb4e172590e23aff0d693cff53af7bc66
Thuraiya <3eya151@gmail.com>
2025-07-28 15:41:31 +0400

    remove commit from list

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -10,16 +10,16 @@ namespace HealthCenterSystem.Models
     {
         public string Spelcialization { get; set; } // property to hold doctor's specialization
         public List<PatientRecord> PatientRecords { get; set; } // list to hold patient records associated with the doctor
-        //public List<Appointment> Appointments { get; set; } // list to hold appointments associated with the doctor
         public List<Clinic> Clinics { get; set; } // list to hold clinics associated with the doctor
-        public List<Doctor> Doctors { get; set; } // list to hold doctors associated with the doctor
         public List<Department> Departments { get; set; } // list to hold departments associated with the doctor
         public Doctor(int id, string name, string email, string password,string Spelcialization)
             :   base(name, email, password, "98376256", "Doctor") // constructor to initialize doctor properties
         {
+            UserId = id; // set the user ID
             this.Spelcialization = Spelcialization; // initialize specialization
             this.PatientRecords = new List<PatientRecord>(); // initialize the list of patient records
             this.Clinics = new List<Clinic>(); // initialize the list of clinics
+            this.Departments = new List<Department>(); // initialize the list of departments
         }
         public void AddPatientRecord(PatientRecord record) // method to add a patient record
         {
