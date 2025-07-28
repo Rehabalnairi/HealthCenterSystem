016675b2cdb41321a47d65da804df3f32e081d01
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-07-28 11:06:58 +0400

    Ibranch service

--- Services/IBranchService.cs
+++ Services/IBranchService.cs
@@ -8,5 +8,9 @@ namespace HealthCenterSystem.Services
 {
     interface IBranchService
     {
+        void AddBranch(string name, string address, string phoneNumber);
+        void UpdateBranch(int id, string name, string address, string phoneNumber);
+        void DeleteBranch(int id);
+        void SetBranchStatus(int id, bool isActive);
     }
 }
